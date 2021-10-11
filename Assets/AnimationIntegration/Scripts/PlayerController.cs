b90eb9dc5f998497e35a114c89072c5e7bbd3f56
nailmailster <nailmailster@gmail.com>
2021-10-11 14:04:03 +0300

    1st backup
    4 анимации и переходы между ними.
    Камера следует за игроком.
    Finishing пока проигрывается прямо на месте

---
+++ Assets/AnimationIntegration/Scripts/PlayerController.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+    float verticalInput, horizontalInput;
+    [SerializeField] float speed = 15;
+    [SerializeField] float rotationSpeed = 90;
+
+    [SerializeField] GameObject enemy;
+    float enemyDistance;
+    [SerializeField] float distanceToFinish = 5;
+
+    [SerializeField] Text instructionsText;
+    bool readyForAction, inAction;
+
+    Vector3 cameraDelta;
+
+    void Start()
+    {
+        cameraDelta = transform.position - Camera.main.transform.position;
+
+        enemy = GameObject.Find("Enemy");
+    }
+
+    void Update()
+    {
+        GetInput();
+        CheckDistanceToEnemy();
+    }
+
+    void GetInput()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space) && readyForAction)
+        {
+            inAction = true;
+            animator.SetTrigger("Finish");
+            StartCoroutine(WaitForFinishing());
+        }
+    }
+
+    IEnumerator WaitForFinishing()
+    {
+        while (!animator.GetCurrentAnimatorStateInfo(0).IsName("Finishing"))
+            yield return null;
+
+        // while (AnimatorIsPlaying() && animator.GetCurrentAnimatorStateInfo(0).IsName("Finishing"))
+        while (animator.GetCurrentAnimatorStateInfo(0).IsName("Finishing"))
+            yield return null;
+
+        inAction = false;
+    }
+
+    bool AnimatorIsPlaying()
+    {
+        return animator.GetCurrentAnimatorStateInfo(0).length > animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
+    }
+
+    void CheckDistanceToEnemy()
+    {
+        if (enemy == null)
+            return;
+
+        enemyDistance = Vector3.Distance(transform.position, enemy.transform.position);
+        readyForAction = !inAction && enemyDistance <= distanceToFinish;
+    }
+
+    private void OnGUI()
+    {
+        instructionsText.gameObject.SetActive(readyForAction);
+    }
+
+    private void FixedUpdate()
+    {
+        if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Finishing"))
+        {
+            animator.SetFloat("Speed", verticalInput);
+
+            transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.fixedDeltaTime);
+
+            Vector3 moveVector = new Vector3(0, 0, verticalInput);
+            transform.Translate(moveVector * speed * Time.fixedDeltaTime, Space.Self);
+
+            Camera.main.transform.position = transform.position - cameraDelta;
+        }
+    }
+}
